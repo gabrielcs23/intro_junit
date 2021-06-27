c474002e9e5efa21b72f3c3b74e111a9dac57951
gabrielcs23 <gabrielcs23@hotmail.com>
2021-06-27 16:52:31 -0300

    inclui testes para os métodos remanescentes de Calculadora

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -1,15 +1,12 @@
 package calculadora;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Classe para teste da calculadora")
 public class CalculadoraTest {
@@ -31,7 +28,7 @@ public class CalculadoraTest {
 	@Test
 	public void testDivisaoDoisNumeros() {
 		int divisao = calc.divisao(8, 4);
-		assertTrue(divisao == 2);
+		assertEquals(2, divisao);
 	}
 	
 	@Test
@@ -48,6 +45,55 @@ public class CalculadoraTest {
 	public void testDivisaoPorZeroComAssertThrows() {
 		assertThrows(ArithmeticException.class,
 				() -> calc.divisao(8, 0));
+	}
+
+	@DisplayName("Testa a subtração de dois números")
+	@Test
+	public void testSubtracaoDoisNumeros() {
+		int sub = calc.subtracao(4, 5);
+		Assertions.assertEquals(-1, sub);
+	}
+
+	@DisplayName("Testa a multiplicação de dois números")
+	@Test
+	public void testMultiplicacaoDoisNumeros() {
+		int multiplicacao = calc.multiplicacao(2, 4);
+		assertEquals(8, multiplicacao);
+	}
+
+	@DisplayName("Testa a somatória até 5")
+	@Test
+	public void testSomatoria() {
+		int somatoria = calc.somatoria(5);
+		assertEquals(15, somatoria);
+	}
+
+	@DisplayName("Testa se o resultado de uma subtração é negativo")
+	@Test
+	public void testSubtracaoResultadoNegativo() {
+		int sub = calc.subtracao(4, 5);
+		assertFalse(calc.ehPositivo(sub));
+	}
+
+	@DisplayName("Testa comparação de números iguais")
+	@Test
+	public void testComparacaoNumerosIguais() {
+		int comparacao = calc.compara(5, 5);
+		assertEquals(0, comparacao);
+	}
+
+	@DisplayName("Testa comparação quando a > b")
+	@Test
+	public void testComparacaoAMaiorQueB() {
+		int comparacao = calc.compara(7, 5);
+		assertEquals(1, comparacao);
+	}
+
+	@DisplayName("Testa comparação quando a < b")
+	@Test
+	public void testComparacaoAMenorQueB() {
+		int comparacao = calc.compara(5, 7);
+		assertEquals(-1, comparacao);
 	}
 
 }
